1a41c4437226a0a196b8c17f507610ec6ec66f88
Leo O'Donnell <leopold.odonnell@gmail.com>
2013-02-24 11:59:58 -0500

    Removed all class variables and broke up methods to simplify complexity.

--- lib/uses_stored_procedures.rb
+++ lib/uses_stored_procedures.rb
@@ -30,69 +30,48 @@ module UsesStoredProcedures
     # and returns an object for the array entry.
     #
     # @raises if the current SQL adapter isn't supported.
-    #
-    
+    #   
     def uses_stored_proc(name, *args, &block)
       options   = args.extract_options!
       proc_name = options[:proc_name] || name.to_s
       
-      self.init_class_attributes()
-      self.install_stored_proc_methods(name, proc_name)
-            
       # Install the row mapper block or method
-      if block_given?      
-        self.stored_proc_block[proc_name] = block
-      elsif options[:filter]
-        self.stored_proc_block[proc_name] = options[:filter]
-      end
+      self.install_stored_proc_methods(name, proc_name, call_stored_proc_verb, options[:filter] || block)
     end    
 
-    # Install variables dynamically to avoid adding it
-    # to ALL instances of ActiveRecord::Base
-    #
-    # :stored_proc_block is a hash that either contains a block or
-    # the symbolic name of the method to call to filter results
-    #
-    # :proc_names is a hash where keys are the method names to call
-    # and the values are the actual stored procedure names
-    # 
-    def init_class_attributes() #:nodoc:
-      return if self.respond_to?(:stored_proc_block)
-      class_attribute :stored_proc_block
-      class_attribute :proc_names
-      class_attribute :call_proc
-      self.stored_proc_block = {}
-      self.proc_names = {}
 
+    # Return the SQL verb for calling a stored procedure.
+    def call_stored_proc_verb()
       case ActiveRecord::Base.connection.adapter_name.to_sym
       when :Mysql2, :MySQL
-        self.call_proc = 'call'
+        'call'
       when :PostgreSQL
-        self.call_proc = 'select'
+        'select'
       when :SQLServer
-        self.call_proc = 'exec'
+        'exec'
       else
         raise "uses_stored_procedurs does not support your connection adapter"
-      end
-        
+      end        
     end
     
     # Define the class and instance methods. Note the need to
     # create a singleton class instance to create the stored
     # procedure.
-    def install_stored_proc_methods(name, proc_name) #:nodoc:
-      self.proc_names[name] = proc_name
-      define_method name do |*args| self.class.exec_stored_proc(proc_names[__method__], args) end
+    def install_stored_proc_methods(name, proc_name, call_stored_proc_verb, filter) #:nodoc:
+      define_method name do |*args| 
+        self.class.exec_stored_proc(proc_name, call_stored_proc_verb, filter, args) 
+      end
+
       singleton_class = class << self; self; end
       singleton_class.send(:define_method, name) do |*args| 
-        exec_stored_proc(proc_names[__method__], args) 
+        exec_stored_proc(proc_name, call_stored_proc_verb, filter, args) 
       end
     end
     
     # Run the stored procedure with the arguments provided and send
     # them through a filter method or block if specified.
-    def exec_stored_proc(name, args) #:nodoc:
-      sql = "#{call_proc} #{name} (" + args.map {|a| "'#{a}'"}.join(',') + ")"
+    def exec_stored_proc(name, call_stored_proc_verb, filter, args) #:nodoc:
+      sql = "#{call_stored_proc_verb} #{name} (" + args.map {|a| "'#{a}'"}.join(',') + ")"
 
       # Call the stored procedure. Note the need to reset the connection
       # because the mysql connection tends to hangup when stored procedures
@@ -102,18 +81,23 @@ module UsesStoredProcedures
       records = ActiveRecord::Base.connection.select_all(sql)
       ActiveRecord::Base.connection.reconnect!
 
-      case stored_proc_block[name]
-        when Symbol
-          # Map with a member function
-          records.map do |r| self.send(stored_proc_block[name].to_s, HashWithAttributes.new.merge(r)) end
-        when Proc
-          # Map with a block
-          records.map do |r| stored_proc_block[name].call(HashWithAttributes.new.merge(r)) end
-        else
-          # Just map it 
-          records.map do |r| HashWithAttributes.new.merge r end
-      end
+      filter_stored_proc_results(records, filter)
     end
+
+    private
+      def filter_stored_proc_results(results, filter)
+        case filter
+          when Symbol
+            # Map with a member function
+            results.map do |r| self.send(filter.to_s, HashWithAttributes.new.merge(r)) end
+          when Proc
+            # Map with a block
+            results.map do |r| filter.call(HashWithAttributes.new.merge(r)) end
+          else
+            # Just map it 
+            results.map do |r| HashWithAttributes.new.merge r end
+        end
+      end
     
   end
 end
